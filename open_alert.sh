34b77f7b58fe6ca1f76692a316de126398cbadcc
JolivetteSax <erikjsax@gmail.com>
2018-02-04 12:02:41 -0500

    added time check for SMS on open alert. Only between 9pm and 8am will the alert be sent

--- open_alert.sh
+++ open_alert.sh
@@ -17,7 +17,16 @@ sleep .05
 # Switch back to undervolt LED test
 echo input | sudo tee /sys/class/leds/led1/trigger
 
-# TODO Check time of day
+HOUR=`date +%H`
+if [ $HOUR -gt 7 ] ; then
+  if [ $HOUR -lt 21 ] ; then
+    echo "Daytime, no SMS"
+    # In order to get alerts at all hours, remove this..
+    exit 0;
+  fi
+fi
+
+#This SMS alert will only be sent if the time is right
 cat phone_numbers.txt | while read line ; do
     ./send_sms.sh $line
 done
